fd2e02191e91a64981daf41cfc58b623c315a9a9
JOAO VITOR DA SILVEIRA SALABERRY <60037410075@senacrs.edu.br>
2026-03-25 09:58:41 -0300

    Exercício lista encadeada em ordem crescente

---
+++ Exercicio/Lista.py
@@ -0,0 +1,42 @@
+from No import No
+
+class Lista:
+    
+    def __init__(self):
+        self.inicio = None
+
+    def imprimir(self):
+        print("----------------")
+        print("Lista encadeada por ordem crescente")
+        if self.inicio is None:
+            print("Lista vazia")
+        else:
+            aux = self.inicio
+            while aux:
+                print( aux.dado )
+                aux = aux.prox
+
+    def add(self, valor):
+        nodo = No(valor)
+        #Inicio vazio? adiciona o nodo aqui em cima
+        if self.inicio == None:
+            self.inicio = nodo
+            # até aqui n muda
+        else:
+            if nodo.dado < self.inicio.dado:
+                nodo.prox = self.inicio
+                self.inicio = nodo
+            else:
+                ant = self.inicio
+                aux = self.inicio.prox
+                while aux:
+                    if nodo.dado < aux.dado:
+                        nodo.prox = aux
+                        ant.prox = nodo
+                        break
+                    else:
+                        ant = aux
+                        aux = aux.prox
+                if aux == None:
+                    ant.prox = nodo
+        self.imprimir()
